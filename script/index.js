93eedd2214f0ff90054fab7aeb5c43d62b612820
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-09-04 16:35:33 +0600

    mehedi

--- script/index.js
+++ script/index.js
@@ -1,1 +1,23 @@
- 
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => display(json.data));
+};
+
+const display = (lesson) => {
+  // console.log(lesson);
+  const levels = document.getElementById("level-container");
+  levels.innerHTML = "";
+  for (const item of lesson) {
+    // console.log(item);
+    const cardLevel = document.createElement("div");
+    cardLevel.innerHTML = `
+          
+          <button class="btn btn-outline btn-primary"
+                  ><i class="fa-solid fa-book-open"></i>lesson-${item.level_no}</button>
+      `;
+    levels.append(cardLevel);
+  }
+};
+
+loadLessons();
